dd81acacd0dd369866c84051e55035e4d73e488e
dylanokelley <dylanokelley@gmail.com>
2024-11-25 20:06:26 -0800

    Trying to get github action to work

--- test_ml.py
+++ test_ml.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from ml.data import process_data
 from ml.model import load_model
+from train_model import model_path
 
 
 def test_rf_classifier():
@@ -11,9 +12,7 @@ def test_rf_classifier():
     Esnure model is RandomForestClassifier
     
     """
-    project_path = "/Users/dylanokelley/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
-    data_path = os.path.join(project_path, "data", "census.csv")
-    model_path = os.path.join(project_path, "model", "model.pkl")
+    #made ajustments to get Github Action to work
     rf_model = load_model(model_path)
     assert isinstance(rf_model, RandomForestClassifier)
 
@@ -22,8 +21,8 @@ def test_for_null():
     """
     Testing for Null Values, as these could severely alter the outcome of ML models
     """
-    project_path = "/Users/dylanokelley/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
-    data_path = os.path.join(project_path, "data", "census.csv")
+
+    data_path = 'data/census.csv'
     data = pd.read_csv(data_path)
     assert sum(data.isnull().sum()) == 0
 
@@ -33,8 +32,6 @@ def test_random_state_42():
     Esnure random state is 42
     
     """
-    project_path = "/Users/dylanokelley/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
-    data_path = os.path.join(project_path, "data", "census.csv")
-    model_path = os.path.join(project_path, "model", "model.pkl")
+
     rf_model = load_model(model_path)
     assert rf_model.random_state == 42
